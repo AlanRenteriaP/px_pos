462688ae782a0368aae5865a3fbd8597c876440f
Alan Renteria <arenteriaperulles@outlook.com>
2023-04-19 18:29:48 -0700

    still fixing typescript

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,40 +1,31 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import {history} from "../redux/helpers";
-import { createTheme , ThemeProvider} from '@mui/material/styles';
-import Alerts from "./components/common/Alerts";
+import { Router, Routes, Route } from 'react-router-dom';
+import { history } from '../redux/helpers';
+import { ThemeProvider } from '@mui/material/styles';
+import Alerts from './components/common/Alerts';
 import PrivateRoute from './components/common/PrivateRoute';
+import theme from './theme';
 
 // Pages
-import Landing from "./pages/landing/Landing";
-import Dashboard from "./pages/dashboard/Dashboard";
-import Auth from "./pages/authentication/Auth";
-import NotFound from "./pages/NotFound/NotFound";
+import Landing from './pages/landing/Landing';
+import Dashboard from './pages/dashboard/Dashboard';
+import Auth from './pages/authentication/Auth';
+import NotFound from './pages/NotFound/NotFound';
 
-function App(){
-    const theme = createTheme({
-        palette: {
-            primary: {
-                main: '#231810'
-            },
-            secondary: {
-                main: "#FCF3C0"
-            }
-        }
-    });
- return(
-        <ThemeProvider theme={theme} >
+function App() {
+    return (
+        <ThemeProvider theme={theme}>
             <Alerts />
-            <Router  history={history}>
+            <Router history={history}>
                 <Routes>
-                    <Route exat path="/" element={<Landing/>}/>
-                    <Route path="/dashboard/*" element={<PrivateRoute><Dashboard/></PrivateRoute>}/>
-                    <Route  path="/auth/*" element={<Auth/>}/>
-                    <Route  path="*" element={<NotFound/>}  />
-                 </Routes>
+                    <Route path="/" element={<Landing />} />
+                    <Route path="/dashboard/*" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+                    <Route path="/auth/*" element={<Auth />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
             </Router>
         </ThemeProvider>
-        );
+    );
 }
 
 export default App;
