d21312fe0c9c0eabf481d3e23f9dc4ab3e583b8d
achi1302 <alexanderson.chacon@gmail.com>
2024-03-27 20:29:47 -0700

    +Two methods in the back-end created, one handles the recipes and the other one the ingredients. The recipes sends the newly created id so it can be grabbed by ingredients. In the Front-end there is a more complete Box that lets the user write a complete recipe with how many ingredients the user wants by creating a for loop manipulating the 'selectedMaterials' and using the recipeId. Form Control to be fixed next, it does not display the other options.

--- src/pages/dashboard/pages/menuManagement/Drawer/CreateMenuItem/CreateMenuItem.tsx
+++ src/pages/dashboard/pages/menuManagement/Drawer/CreateMenuItem/CreateMenuItem.tsx
@@ -1,6 +1,7 @@
 import React, { FormEvent, useState, useEffect } from 'react';
 import RawMaterialForm from './RawMaterialForm';
-import { Paper, Box, TextField, Button } from '@mui/material';
+import { Paper, Box, TextField, Button, Select, MenuItem, InputLabel, FormControl, SelectChangeEvent } from '@mui/material';
+
 
 export type RawMaterial = {
     id: number;
@@ -21,6 +22,11 @@ const CreateMenuItem = ({ onClose }: Props) => {
     const [selectedMaterials, setSelectedMaterials] = useState<MaterialType[]>([]);
     const [itemName, setItemName] = useState('');
     const [itemDescription, setItemDescription] = useState('');
+    const [itemPrice, setItemPrice] = useState('');
+    const [itemCategory, setItemCategory] = useState<string[]>(['food']);
+
+
+
 
     useEffect(() => {
         console.log('Updated selectedMaterials:', selectedMaterials);
@@ -33,31 +39,63 @@ const CreateMenuItem = ({ onClose }: Props) => {
     const handleItemDescriptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setItemDescription(event.target.value);
     };
+    const handleItemPriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setItemPrice(event.target.value);
+    };
+    const handleItemCategoryChange = (event: SelectChangeEvent<string[]>) => {
+        setItemCategory(event.target.value as string[]);
+    };
 
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
         const payload = {
             name: itemName,
             description: itemDescription,
-            materials: selectedMaterials,
+            price: itemPrice,
+            categories: itemCategory
         };
 
         console.log(payload);
-        fetch('http://localhost:8080/menuBuild/add_menu_item', {
+        let response = await fetch('http://localhost:8080/menumanagement/add_recipe', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(payload),
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Success:', data);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
+        });
+
+        let data = await response.json();
+        if (data.error) {
+            console.error('Error:', data.error);
+            return;
+        }
+        console.log('Success:', data);
+
+        const recipeId = data.recipeId;
+
+        for (let material of selectedMaterials) {
+            let payloadIng = {
+                recipeId,
+                productId: material.material.id,
+                quantity: material.quantity
+            };
+            response = await fetch('http://localhost:8080/menumanagement/add_ingredient', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(payloadIng),
             });
+
+            data = await response.json();
+            if (data.error) {
+                console.error('Error:', data.error);
+                return;
+            }
+            console.log('Success:', data);
+        }
+
         onClose();
     };
 
@@ -79,6 +117,35 @@ const CreateMenuItem = ({ onClose }: Props) => {
                         value={itemDescription}
                         onChange={handleItemDescriptionChange}
                     />
+                    <TextField
+                        label="Price"
+                        placeholder="Item Price"
+                        multiline
+                        value={itemPrice}
+                        onChange={handleItemPriceChange}
+                    />
+
+                    {/*<TextField*/}
+                    {/*    label="Category"*/}
+                    {/*    placeholder="Item Category"*/}
+                    {/*    multiline*/}
+                    {/*    value={itemCategory}*/}
+                    {/*    onChange={handleItemCategoryChange}*/}
+                    {/*/>*/}
+
+                    <FormControl>
+                        <InputLabel>Category</InputLabel>
+                        <Select
+                            value={itemCategory}
+                            onChange={handleItemCategoryChange}
+                            multiple
+                        >
+                            <MenuItem value="food">food</MenuItem>
+                            <MenuItem value="alcohol">alcohol</MenuItem>
+                            <MenuItem value="drinks">drinks</MenuItem>
+                            <MenuItem value="pastry">pastry</MenuItem>
+                        </Select>
+                    </FormControl>
 
                     <RawMaterialForm
                         selectedMaterials={selectedMaterials}
